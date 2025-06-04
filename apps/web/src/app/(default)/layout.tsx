71fb1283d5cdb41cf4c051fbf048baee689b12a3
Fred Pope <fred@fredpope.com>
2025-06-04 13:57:26 -0600

    Updates to Layout and Middleware.

---
+++ apps/web/src/app/(default)/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from "@clerk/themes";
+import { ConditionalLayout } from "@/components/ConditionalLayout";
+import { Toaster } from 'sonner';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GLAPI App", // Updated title
+  description: "API First General Ledger", // Updated description
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en" className="dark" suppressHydrationWarning>
+        <body className={`${inter.className} bg-background text-foreground`}>
+          <ConditionalLayout>
+            {children}
+          </ConditionalLayout>
+          <Toaster richColors closeButton position="top-right" />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
