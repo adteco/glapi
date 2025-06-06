cfe2f191fd5e77e5e30d952ff4b02d6680f9dca3
Fred Pope <fred@fredpope.com>
2025-06-06 11:25:41 -0600

    Updates

--- apps/docs/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-// Disable middleware entirely for the docs app
-// This ensures no request interception occurs
-export default function middleware() {
-  // Middleware is disabled
-}
-
-export const config = {
-  matcher: [],
-}
